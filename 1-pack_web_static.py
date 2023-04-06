c945be8059bcf10189700d274b9c86ab3a171579
Sonlowami <uwilowami64@outlook.com>
2023-04-06 20:23:27 +0200

    Solve task 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,7 +5,7 @@
 def do_pack():
     """Pack all html directories into a tar.gz file"""
     from fabric.api import local
-    from  datetime import datetime
+    from datetime import datetime
     now = datetime.now()
     local('mkdir -p versions')
     pth = 'versions/web_static_{}{}{}{}{}{}'.format(now.year, now.month,
@@ -14,5 +14,3 @@ def do_pack():
     result = local('tar -czvf {}.tgz web_static/'.format(pth))
     if result.succeeded:
         return pth
-
-
---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module contain a function to deploy web static to a
+web server"""
+from fabric.api import env
+
+
+def do_deploy(archive_path):
+    """Deploy from an archive path given. Servers are specified at the
+    commandline with fab"""
+    from os.path import isfile
+    from fabric.api import run, put, sudo
+
+    if not isfile(archive_path):
+        return False
+    put(archive_path, '/tmp/', use_sudo=True, mirror_local=True)
+    try:
+        sudo('tar -xvzf /tmp/web_static_*.tgz -C /data/web_static/releases/')
+        sudo('rm /tmp/web_static_*.tgz /data/web_static/current')
+        run(
+            'ln -s $(ls -t /data/web_static/releases|head -n 1)\
+                    /data/web_static/current')
+        return True
+    except Exception:
+        return False
+
+
+env.hosts = ['54.87.205.95', '54.237.226.36']
