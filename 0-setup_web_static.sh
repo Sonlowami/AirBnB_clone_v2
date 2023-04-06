89e5e2ab0515cdf8aa31efefe4b92e5c6d4b3256
Sonlowami <uwilowami64@outlook.com>
2023-04-06 18:37:28 +0200

    Force creation of new link

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# This script sets the environment for deployment on my web servers
+#
+# Step 1: Install nginx if not present
+apt-get update
+apt-get upgrade -y
+apt install nginx
+
+# Step 2: Set up required directories and files
+mkdir -p /data/web_static/releases/test
+mkdir /data/web_static/shared
+printf "Welcome to my AirBnB\n" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown --recursive ubuntu:ubuntu /data
+sed -i '^\tserver_name.*/a \n\\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\ttry_files $uri $uri/ =404;\n}' /etc/nginx/sites-enabled/default
+
---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script contains a function to archieve the webstatic folder"""
+
+
+def do_pack():
+    """Pack all html directories into a tar.gz file"""
+    from fabric.api import local
+    from  datetime import datetime
+    now = datetime.now()
+    local('mkdir -p versions')
+    pth = 'versions/web_static_{}{}{}{}{}{}'.format(now.year, now.month,
+                                                    now.day, now.hour,
+                                                    now.minute, now.second)
+    result = local('tar -czvf {}.tgz web_static/'.format(pth))
+    if result.succeeded:
+        return pth
+
+
