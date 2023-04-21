66216139665cf2a3b9e607cb11ca99e903aa3985
Sonlowami <uwilowami64@outlook.com>
2023-04-21 14:43:42 +0200

    Fix format string errors in other files

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -13,20 +13,20 @@ def index():
     """
     Return a simple hello HBNB message
     """
-    return "Hello HBNB!\n"
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Return a simple HBNB message"""
-    return "HBNB\n"
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def print_c(text):
     """Return the parameter passed in a text"""
     new_text = text.replace('_', ' ')
-    return f"C {escape(new_text)}\n"
+    return "C {}".format(escape(new_text))
 
 
 @app.route('/python/', strict_slashes=False)
@@ -34,7 +34,7 @@ def print_c(text):
 def print_python(text='is cool'):
     """Prints passed text, is cool if not passed"""
     new_text = text.replace('_', ' ')
-    return f"Python {escape(new_text)}\n"
+    return "Python {}".format(escape(new_text))
 
 
 @app.route('/number/<n>', strict_slashes=False)
@@ -42,7 +42,7 @@ def print_number(n):
     """Print a n if is a number"""
     try:
         if isinstance(int(n), int):
-            return f"{n} is a nummber\n"
+            return "{} is a nummber".format(n)
     except Exception:
         abort(404)
 
@@ -52,7 +52,7 @@ def number_template(n):
     """ Display an html page only if n is an integer"""
     try:
         if isinstance(int(n), int):
-            return render_template('5-number.html', number=n)
+            return render_template('5-number.html', number=int(n))
     except Exception:
         abort(404)
 
